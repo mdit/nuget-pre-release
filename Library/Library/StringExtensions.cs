719a366dd9e34cee2ab2a6f371567ad54cddd7b3
Matt Davidson <matt.davidson@forceindiaf1.com>
2018-11-21 21:40:43 +0000

    Adds library

---
+++ Library/Library/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library
+{
+    public static class StringExtensions
+    {
+        public static void Print(this string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
