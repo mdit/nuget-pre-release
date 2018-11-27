da04c485a08c5e74c04b2f7aae0331df063f8726
Matt Davidson <matt.davidson@forceindiaf1.com>
2018-11-27 22:37:55 +0000

    Changes consumer to use pre-release package

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -7,7 +7,7 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            "Hello World!".Print();
         }
     }
 }
