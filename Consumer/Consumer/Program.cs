7100ae1b3a65e885b6d900ab14851d89b754b202
Matt Davidson <matt.davidson@forceindiaf1.com>
2018-11-27 22:27:52 +0000

    Adds Consumer solution

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Library;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
